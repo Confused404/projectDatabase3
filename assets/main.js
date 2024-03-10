329c1cbb68160870a0cd5a4d06e791c877e69456
paulvinueza30 <paulvinueza30@gmail.com>
2024-03-10 18:49:41 -0400

    added same functionality as the login button onto the signup button, but still needs modifications to make it unique

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,32 @@
-var loginForm = document.getElementsByClassName("form-div")[0];
-var loginButton = document.getElementById("login-button");
+// varform = document.getElementsByClassName("form-div")[0];
+// var loginButton = document.getElementById("login-button");
 
-loginButton.addEventListener("click", function () {
-  if (loginForm.offsetHeight == 0) {
-    loginForm.style.display = "block";
-    loginButton.disabled = true;
-  }
+// loginButton.addEventListener("click", function () {
+//   if form.offsetHeight == 0) {
+//   form.style.display = "block";
+//     loginButton.disabled = true;
+//   }
+// });
+
+let buttons = document.querySelectorAll(".user");
+
+// Add event listener to each button
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    let formId = button.id;
+    let form;
+    if (formId === "signup") {
+      form = document.querySelector('form[action="signup"]');
+    } else if (formId === "login") {
+      form = document.querySelector('form[action="login"]');
+    }
+    let parent = form.parentNode;
+    if (parent.offsetHeight == 0) {
+      parent.style.display = "block";
+      buttons.forEach(function (button) {
+        button.disabled = true;
+      });
+    }
+  });
 });
